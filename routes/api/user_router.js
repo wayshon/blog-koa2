a602bf2f5636aff00447eb081bc3f21226938800
santong <santongdbs@163.com>
2017-06-28 10:06:17 +0800

    fix url

--- routes/api/user_router.js
+++ routes/api/user_router.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')();
 const userController = require('../../app/controller').user;
 
-router.post('/', (ctx, next) => userController.login(ctx, next));
+router.post('/login', (ctx, next) => userController.login(ctx, next));
 
 router.get('/test', function (ctx, next) {
   ctx.body = 'this is a users/test response'
