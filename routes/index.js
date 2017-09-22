2e8d6319a4f471018d0792eb67df791560b0a2c6
王旭 <wayshon@wangxudeMacBook-Pro.local>
2017-09-22 14:14:07 +0800

    upload

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,12 @@ router.get('/', async (ctx, next) => {
   ctx.body = "blog api"
 })
 
+router.get('/eat', async (ctx, next) => {
+  await ctx.render('what-eat')
+})
+
+router.get('/upload', async (ctx, next) => {
+  await ctx.render('upload')
+})
+
 module.exports = router;
