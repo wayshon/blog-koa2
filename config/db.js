bf22cd33914e86536586052a6470f769268a53e9
wayshon <wayshon@qq.com>
2018-10-16 16:54:31 +0800

    remove config

--- config/db.js
+++
@@ -1,5 +0,0 @@
-// MySQL数据库联接配置
-let {mysqlConfig} = require('../config');
-module.exports = {
-    mysql: mysqlConfig
-};
--- config/index.js
+++
@@ -1,8 +0,0 @@
-const developmentEnv = require('./Development');
-const testEnv = require('./Test');
-
-//根据不同的NODE_ENV，输出不同的配置对象，默认输出development的配置对象
-module.exports = {
-    development: developmentEnv,
-    test: testEnv
-}[process.env.NODE_ENV || 'development']
